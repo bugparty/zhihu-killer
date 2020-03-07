a85716c090219ec2f8d4756061562b3b0b7a3a47
ClayM0re <fancycode@gmail.com>
2020-03-07 14:13:09 +0800

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         知乎剪贴板杀手
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  去掉复制知乎答案时的版权提示和字数限制（注意：仅用于个人笔记用途，请遵守版权协议，如果您认为侵犯了您的权益，请通知我删除）
+// @version      1.04
+// @description  去掉复制知乎答案的限制（注意：仅用于个人笔记用途，请遵守版权协议）
 // @author       bugparty    
 // @home-url     https://github.com/bugparty/zhihu-killer
 // @license      MIT License
@@ -15,14 +15,14 @@
 
 
 // 选取回答正文外部的span标签
-var targetSelector = ".QuestionAnswer-content  span.RichText"
+var targetSelector = ".QuestionAnswer-content  span.RichText, span.CopyrightRichText-richText"
 
 // need to wait for article content loaded after ajax request
 waitForKeyElements (targetSelector, cleanClipboard);
 
 function cleanClipboard() {
     'use strict';
-
+		console.log("zhihu killer find a new questino to mask")
     // 由于在 div[id=article-content] 处监听所有冒泡到此处的 copy 事件，所有可以在下一个子节点拦截。
     // 参考：https://ghoulmind.com/2016/02/remove-zhihu-copyright-on-copy/
 
