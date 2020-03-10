62530310f830b7e3b4bc7629955bfa1e30a265c7
ClayM0re <fancycode@gmail.com>
2020-03-10 21:09:51 +0800

    fix bug

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎和谐卫士
 // @namespace    http://tampermonkey.net/
-// @version      1.05
+// @version      1.10
 // @description  去掉连接重定向, 去掉复制知乎答案的限制（注意：仅用于个人笔记用途，请遵守版权协议）
 // @author       bugparty    
 // @home-url     https://github.com/bugparty/zhihu-killer
@@ -13,27 +13,22 @@
 // @note         
 // ==/UserScript==
 
-
 // 选取回答正文外部的span标签
-var targetSelector = ".QuestionAnswer-content  span.RichText, span.CopyrightRichText-richText"
-
+var targetSelector = "div.RichContent-inner > span"
 // need to wait for article content loaded after ajax request
 waitForKeyElements (targetSelector, cleanClipboard);
 
 function cleanClipboard() {
     'use strict';
-		console.log("zhihu killer find a new questino to mask")
+    console.log("zhihu killer find a new questino to mask")
     // 由于在 div[id=article-content] 处监听所有冒泡到此处的 copy 事件，所有可以在下一个子节点拦截。
     // 参考：https://ghoulmind.com/2016/02/remove-zhihu-copyright-on-copy/
-    let elements = document.querySelectorALL(targetSelector);
-     elements.forEach(function(e){
-        e.stopPropagation();
-     });
-  
+    $(targetSelector).on('copy', e => e.stopPropagation());
 }
-waitForKeyElements ("a.external", function(item){
- 
-  let elements = document.querySelectorAll("a.external");
+
+function transformLinks(){
+  'use strict';
+  var elements = document.querySelectorAll("a.external");
   elements.forEach(function(e){
     if(e.href.indexOf("https://link.zhihu.com/?target=") == 0){
         let new_link = decodeURIComponent(e.href.replace("https://link.zhihu.com/?target=", ""));
@@ -41,4 +36,6 @@ waitForKeyElements ("a.external", function(item){
         console.log("zhihu killer, mask a new link", new_link);
      }
   });
-});
+}
+waitForKeyElements ("a.external",transformLinks);
+$(document).ready(transformLinks);
