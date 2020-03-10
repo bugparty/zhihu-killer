7cba85cb1c54b983613a6b8bfa0ef1db3ec88d5d
ClayM0re <fancycode@gmail.com>
2020-03-10 18:25:16 +0800

    add remove link redirection to external

--- userscript.js
+++ userscript.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         知乎剪贴板杀手
+// @name         知乎和谐卫士
 // @namespace    http://tampermonkey.net/
-// @version      1.04
-// @description  去掉复制知乎答案的限制（注意：仅用于个人笔记用途，请遵守版权协议）
+// @version      1.05
+// @description  去掉连接重定向, 去掉复制知乎答案的限制（注意：仅用于个人笔记用途，请遵守版权协议）
 // @author       bugparty    
 // @home-url     https://github.com/bugparty/zhihu-killer
 // @license      MIT License
-// @match        https://www.zhihu.com/question/*
+// @match        https://www.zhihu.com/*
 // @grant        unsafeWindow
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js
 // @require      https://greasyfork.org/scripts/5392-waitforkeyelements/code/WaitForKeyElements.js?version=19641
@@ -25,8 +25,20 @@ function cleanClipboard() {
 		console.log("zhihu killer find a new questino to mask")
     // 由于在 div[id=article-content] 处监听所有冒泡到此处的 copy 事件，所有可以在下一个子节点拦截。
     // 参考：https://ghoulmind.com/2016/02/remove-zhihu-copyright-on-copy/
-
-    $(targetSelector).on('copy', function(evt) {
-        evt.stopPropagation();
-    });
+    let elements = document.querySelectorALL(targetSelector);
+     elements.forEach(function(e){
+        e.stopPropagation();
+     });
+  
 }
+waitForKeyElements ("a.external", function(item){
+ 
+  let elements = document.querySelectorAll("a.external");
+  elements.forEach(function(e){
+    if(e.href.indexOf("https://link.zhihu.com/?target=") == 0){
+        let new_link = decodeURIComponent(e.href.replace("https://link.zhihu.com/?target=", ""));
+        e.href = new_link;
+        console.log("zhihu killer, mask a new link", new_link);
+     }
+  });
+});
